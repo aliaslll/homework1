96ef46f1ef574f9f766db0283df7bb56982877dd
Natalia demetradze <demetradzenatalia16@gmail.com>
2025-08-31 14:11:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let item = 100;
+if (item < 50 ) {
+    console.log("< 50")
+} else if ( item > 20) {
+    console.log("> 20")
+} else {
+    console.log("false")
+}
+
+let accessAllowed;
+let nameUser = mariam;
+if (nameUser = mariam) {
+    accessAllowed = true;
+} else {
+    accessAllowed = false;
+}
+alert(accessAllowed);
+
+
+switch(user) {
+    case mariam:
+        console.log("true")
+        break;
+    default:
+        console.log("false")
+}
